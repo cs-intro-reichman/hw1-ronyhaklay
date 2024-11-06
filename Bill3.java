3a3482543c6f9f3609730a9664d0fbac0fbfd46f
רוני חקלאי <ronyhaklay@Air-sl-rwny.lan>
2024-11-06 21:56:06 +0200

    second bill3

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
 		String name3 = args[2]; 
 		int numbill = Integer.parseInt(args[3]);
 		double total = Math.ceil((double)numbill/3);
-		System.out.print("Dear " + name1 + ", " + name2 + " and " + name3 + ": " + "pay " + total + " shekels each.");
+		System.out.print("Dear " + name3 + ", " + name2 + " and " + name1 + ": " + "pay " + total + " shekels each.");
 	}
 }
